34139e74b6be6194f780b73e66a533692b2a7c24
David <david.netusil@email.cz>
2024-04-22 14:37:48 +0200

    AboutPage finished

--- WebsiteForNbaFans/client-app/src/pages/AboutPage.vue
+++ WebsiteForNbaFans/client-app/src/pages/AboutPage.vue
@@ -1,6 +1,34 @@
 <template>
-  <div>
-    <h1>ABOUT PAGE</h1>
+  <div class="flex justify-center bg-white mt-1">
+    <h1 class="text-2xl">
+      O nás
+    </h1>
+  </div>
+  <div class="flex justify-center">
+    <div class="w-1/2 bg-white mt-1">
+      <main class="p-5">
+        <p><b>Tato stránka vám přináší nejnovější informace on NBA!</b></p>
+        <p class="mt-5">
+          Najdete zde jednotlivé zápasy hrané za poslední dny a jejich výsledky. Dále se zde nachází část pro novinky a články.
+          Jako každá sportovní webová stránka se zde nachází i statistická část. V té najdete veškeré základní statistiky NBA. Statistiky jsou rozdělené na týmy a jednotlivé hráče.
+        </p>
+        <p class="mt-5">          
+          Vznikla jako projekt pro bakalářskou práci. Jsem dlouhodobý fanoušek NBA. Sám jsem kdysi hrával basketbal za Sokol Vyšehrad na úrovni žáků a dorostu.
+          Během výběru bakalářské práce jsem chtěl vytvořit webovou stránku, kterou jsem sám postrádal, tehdy když jsem byl ještě malý basketbalový fanoušek. 
+          Tehdy jsem nerozumněl anglicky na tolik dobře, abych mohl používat oficiální stránku NBA. 
+          Proto jsem se rozhodl, že svojí bakalářskou práci budu chtít věnovat právě basketbalu. Tato stránka využívá ASP.NET pro backend a Vue.js pro frontend.          
+        </p>
+        <p class="mt-5">
+          Veškerá data, které vidíte na této stránce pochází z veřejně dostupné NbaAPI od RapidAPI. Články jsou převzaté z jiných webových stránek pro potřebu bakalářské práce a žádné z nich nevlastním.
+        </p>
+        <p class="mt-5">
+          Pokud jste dočetli až jsem Děkuji za váš čas.          
+        </p>
+        <p>
+          David Netušil
+        </p>
+      </main>
+    </div>
   </div>
 </template>
 
